8afcc5b773047be0bb209263b008eff9f25c6103
KuanYuLai <yorklai0523@gmail.com>
2017-06-12 05:29:19 -0700

    Building server with api

--- server.js
+++ server.js
@@ -4,8 +4,10 @@
  var exphbs = require("express-handlebars");
 
  var weather = require("./weather");
+ var zip = require("./zip");
  var app = express();
  var port = process.env.PORT || 3000;
+ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
  app.engine('handlebars', exphbs({defaultLayout: 'main'}));
  app.set('view engine', 'handlebars');
@@ -18,14 +20,39 @@
 
 app.use(express.static(path.join(__dirname, 'v0/public')));
 
- app.get('/', function(req, res, next){
-   var templateArgs = {
-     location: weather["main"].location,
-     date: weather["main"].date,
-     spot: weather["sub"]
-   }
-       res.render('weatherPage', templateArgs);
+app.get('/', function(req, res, next){
+   /*zip["sub"].forEach(function(zipcode){
+     httpGet("http://api.openweathermap.org/data/2.5/weather?zip="
+    + zipcode +",us&appid=01d189351de6cfc4bf0155a1e9734f03&&units=imperial");
+  });*/
+  var arg = httpGet();
+  console.log(arg["spot"].temperature);
+  res.render('weatherPage', arg);
   });
+
+  function httpGet()
+  {
+    var xhr = new XMLHttpRequest(),
+    method = "GET",
+    url = "http://api.openweathermap.org/data/2.5/weather?zip=97331,us&appid=01d189351de6cfc4bf0155a1e9734f03&&units=imperial";
+
+    xhr.open(method, url, true);
+    xhr.onreadystatechange = function () {
+    if(xhr.readyState == 4) {
+      //console.log(xhr.responseText);
+      var weather = JSON.parse(xhr.responseText);
+      var templateArgs = {
+        local: weather.main,
+        spot: {
+          temperature: weather.main.temp,
+          location: weather.name
+        }
+      }
+      return templateArgs;
+    }
+  };
+  //xhr.send();
+  }
 
  app.listen(port, function(err){
  	console.log("Server runing on port 3000");
--- v0/public/main.js
+++ v0/public/main.js
@@ -1,26 +1,24 @@
-var addLocation = document.getElementById('addLocation');
+//01d189351de6cfc4bf0155a1e9734f03
+document.getElementById('addLocation').addEventListener("click", show);
 var backdrop = document.getElementById('backdrop');
 var popout = document.getElementById('popout');
-var closeBtn = document.querySelector('.close-btn');
-var cancelBtn = document.querySelector('.cancel-btn');
+document.querySelector('.add-btn').addEventListener('click', add);
+document.querySelector('.close-btn').addEventListener('click', hide);
+document.querySelector('.cancel-btn').addEventListener('click', hide);
 
-addLocation.addEventListener('click', function(){
-
+function show() {
   backdrop.classList.remove('hidden');
   popout.classList.remove('hidden');
+}
 
-});
-
-closeBtn.addEventListener('click', function(){
-
+function hide(){
   backdrop.classList.add('hidden');
   popout.classList.add('hidden');
+  document.getElementById('zip-input').value = "";
+}
 
-});
-
-cancelBtn.addEventListener('click', function(){
-
-  backdrop.classList.add('hidden');
-  popout.classList.add('hidden');
-
-});
+function add(){
+  var url = "http://api.openweathermap.org/data/2.5/weather?zip=97331,us&appid=01d189351de6cfc4bf0155a1e9734f03&&units=imperial";
+  loadJSON(url, gotdata);
+  location.reload();
+}
