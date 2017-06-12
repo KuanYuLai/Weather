244dc1b59be3ec57509590cef42fe6a0a05157bb
KuanYuLai <yorklai0523@gmail.com>
2017-06-12 16:09:58 -0700

    forecast temp and location link with api

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ var fs = require("fs");
 var express = require("express");
 var exphbs = require("express-handlebars");
 
-var weather = require("./weather");
+//var weather = require("./weather");
 var zip = require("./zip");
 var app = express();
 var port = process.env.PORT || 3000;
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var templatesArgs;
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -15,42 +16,44 @@ app.set('view engine', 'handlebars');
 //statically serve v0/public files
 app.use(express.static(path.join(__dirname, 'v0/public')));
 
+function httpGet(callback) {
+  var xhr = new XMLHttpRequest(),
+  url = "http://api.openweathermap.org/data/2.5/weather?zip=97331,us&appid=01d189351de6cfc4bf0155a1e9734f03&&units=imperial";
+
+  xhr.open('GET', url, true);
+  xhr.send();
+
+  xhr.onreadystatechange = function() {
+    if(this.readyState == 4) {
+      var weather = JSON.parse(this.responseText);
+      callback(weather);
+      }
+
+  };
+return xhr.onreadystatechange();
+}
+
 //main handler
 app.get('/', function(req, res, next){
    /*zip["sub"].forEach(function(zipcode){
      httpGet("http://api.openweathermap.org/data/2.5/weather?zip="
     + zipcode +",us&appid=01d189351de6cfc4bf0155a1e9734f03&&units=imperial");
   });*/
-  var arg = httpGet();
-  console.log(arg["spot"].temperature);
-  res.render('weatherPage', arg);
+
+
+  httpGet(function render(weather){
+      templatesArgs = {
+        local: {
+          temp: weather.main.temp | 0,
+          location: weather.name
+        }
+        }
+        res.render('weatherPage', templatesArgs);
+        })
 });
 
-function httpGet() {
-  var xhr = new XMLHttpRequest(),
-  method = "GET",
-  url = "http://api.openweathermap.org/data/2.5/weather?zip=97331,us&appid=01d189351de6cfc4bf0155a1e9734f03&&units=imperial";
-
-  xhr.open(method, url, true);
-  xhr.onreadystatechange = function () {
-    if(xhr.readyState == 4) {
-      //console.log(xhr.responseText);
-      var weather = JSON.parse(xhr.responseText);
-      var templateArgs = {
-        local: weather.main,
-        spot: {
-          temperature: weather.main.temp,
-          location: weather.name
-        },
-      };
-      return templateArgs;
-    }
-  };
-//xhr.send();
-}
-
 //post handler
-app.post( , function(req, res, next) {
+/*app.post( , function(req, res, next) {
 
   fs.writeFile('zip.json', JSON.stringify(zipData), function(err) {
     if (err) {
@@ -59,7 +62,9 @@ app.post( , function(req, res, next) {
       res.status(200).send();
     }
   });
-});
+});*/
+
+
 
 //404 handler
 app.get('*',function(req,res,next){
