8feec7e360dc5703e34a354b92dc6c0afbd43b6b
Kevin Koos <narwhal.kjk@gmail.com>
2017-06-12 12:35:10 -0700

    minor changes - templateArgs spot property needs fixing

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
- var path = require("path");
- var fs = require("fs");
- var express = require("express");
- var exphbs = require("express-handlebars");
+var path = require("path");
+var fs = require("fs");
+var express = require("express");
+var exphbs = require("express-handlebars");
 
- var weather = require("./weather");
- var zip = require("./zip");
- var app = express();
- var port = process.env.PORT || 3000;
- var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var weather = require("./weather");
+var zip = require("./zip");
+var app = express();
+var port = process.env.PORT || 3000;
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
- app.engine('handlebars', exphbs({defaultLayout: 'main'}));
- app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 
- app.get('/twits/:index', function(req, res, next){
-   console.log("==url params for request: ", req.params);
-    next();
- });
+app.get('/twits/:index', function(req, res, next){
+ console.log("==url params for request: ", req.params);
+  next();
+});
 
+//statically serve v0/public files
 app.use(express.static(path.join(__dirname, 'v0/public')));
 
+//main handler
 app.get('/', function(req, res, next){
    /*zip["sub"].forEach(function(zipcode){
      httpGet("http://api.openweathermap.org/data/2.5/weather?zip="
@@ -28,16 +30,15 @@ app.get('/', function(req, res, next){
   var arg = httpGet();
   console.log(arg["spot"].temperature);
   res.render('weatherPage', arg);
-  });
+});
 
-  function httpGet()
-  {
-    var xhr = new XMLHttpRequest(),
-    method = "GET",
-    url = "http://api.openweathermap.org/data/2.5/weather?zip=97331,us&appid=01d189351de6cfc4bf0155a1e9734f03&&units=imperial";
+function httpGet() {
+  var xhr = new XMLHttpRequest(),
+  method = "GET",
+  url = "http://api.openweathermap.org/data/2.5/weather?zip=97331,us&appid=01d189351de6cfc4bf0155a1e9734f03&&units=imperial";
 
-    xhr.open(method, url, true);
-    xhr.onreadystatechange = function () {
+  xhr.open(method, url, true);
+  xhr.onreadystatechange = function () {
     if(xhr.readyState == 4) {
       //console.log(xhr.responseText);
       var weather = JSON.parse(xhr.responseText);
@@ -47,13 +48,19 @@ app.get('/', function(req, res, next){
           temperature: weather.main.temp,
           location: weather.name
         }
-      }
+      };
       return templateArgs;
     }
   };
-  //xhr.send();
-  }
+//xhr.send();
+}
 
- app.listen(port, function(err){
- 	console.log("Server runing on port 3000");
- });
+//404 handler
+app.get('*',function(req,res,next){
+  res.status(404);
+  res.render("404Page");
+});
+
+app.listen(port, function(err){
+	console.log("Server runing on port: " + port);
+});
